4aca7ec59b65b3a08168e82c262b8c72b65895a6
Chris Bibby <cmbibby@gmail.com>
2019-09-13 11:27:40 +0800

    check wp version on activate

--- plugin.php
+++ plugin.php
@@ -23,3 +23,18 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/options-page.php';
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+/**
+ * Register activation hook and only activate plugin if we have v5 or greater
+ */
+
+function mbtb_activate() {
+  $wp_version = get_bloginfo( 'version' );
+	if ( ! version_compare( $wp_version, '5.0', '>=' ) ) {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		wp_die( 'This plugin requires the block editor in WordPress v5.0 and above, please update to the latest WordPress version.' . version_compare( $wp_version, '5.0', '>=' ) );
+	}
+}
+
+register_activation_hook( __FILE__, 'mbtb_activate' );
+
